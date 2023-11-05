649012f4ee56c883d3510edfb214a02bdc5601fd
ahmedwashere <aeldeso@xula.edu>
2023-11-05 09:21:33 -0600

    Added API functionality

--- Public/app.ts
+++ Public/app.ts
@@ -1,24 +1,40 @@
-﻿const message: string = 'Hello, World from Typescript';
-console.log(message);
+﻿interface Item {
+    company?: string; // Use ? for optional properties if not all your items have a company
+    name: string;
+    price: string;
+    imagePath: string;
+}
 
 async function fetchData() {
     try {
-        // Make sure you use the correct protocol and IP address format
-        const response = await fetch('http://127.0.0.1:5000/api/data');
-        const data = await response.json();
-        console.log(data);
-        displayData(data['message']);
+        // Replace with '/api/products' or '/api/restaurants' as necessary
+        const response = await fetch('http://127.0.0.1:5000/api/products');
+        const items: Item[] = await response.json();
+        displayItems(items);
     } catch (error) {
         console.error('Error fetching data: ', error);
     }
 }
 
-function displayData(message: string) {
-    const appDiv: HTMLElement | null = document.getElementById('app');
-    if (appDiv) {
-        // Use backticks for template literals
-        appDiv.innerHTML = `<h2>${message}</h2>`;
+function displayItems(items: Item[]) {
+    const itemsContainer: HTMLElement | null = document.getElementById('items-container');
+    if (itemsContainer) {
+        itemsContainer.innerHTML = ''; // Clear out any existing content
+        items.forEach((item) => {
+            const itemCard = `
+                <div class="card">
+                    <img src="${item.imagePath}" alt="${item.name}" />
+                    <div class="card-body">
+                        <h3 class="card-title">${item.name}</h3>
+                        <p class="card-text">${item.company || ''}</p>
+                        <p class="price">$${item.price}</p>
+                    </div>
+                </div>
+            `;
+            itemsContainer.innerHTML += itemCard; // Append new card
+        });
     }
 }
 
 fetchData();
+
